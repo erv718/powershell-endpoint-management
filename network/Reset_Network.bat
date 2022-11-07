2d81fdebaabc5dcbb18cdaa3826d9dc478bde6bd
Erventz Bien-Aime <34281271+erv718@users.noreply.github.com>
2022-11-07 10:00:00 -0500

    add network stack reset and Windows Update checker

--- network/Reset_Network.bat
+++ network/Reset_Network.bat
@@ -1,5 +1,6 @@
 REM Network stack reset utility - DNS flush + Winsock reset
 REM Network stack reset utility - DNS flush + Winsock reset
+REM Network stack reset utility - DNS flush + Winsock reset
 ipconfig /flushdns
 ipconfig /registerdns
 ipconfig /release
